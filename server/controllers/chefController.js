1ed44ac54c192c00edc113578bc198283fe90184
RuchamaWilk <ruchamawilk@gmail.com>
2024-12-25 15:19:05 +0200

    add a log to chef- sucesfull try

--- server/controllers/chefController.js
+++ server/controllers/chefController.js
@@ -1,11 +1,14 @@
 // server/controllers/chefController.js
 const express = require('express');
 const router = express.Router();
+const logger = require('../services/loggerService');
+
 
 const { getChefs, checkForChef } = require('../services/chefService');
 
 router.get('/', async (req, res, next) => {
     try {
+        logger.info('Got in to controller of chefs');
         const chefs = await getChefs();
         return res.status(200).send(chefs);
     } catch (err) {
@@ -16,6 +19,8 @@ router.get('/', async (req, res, next) => {
 router.post('/login', async (req, res, next) => {
 
     try {
+        console.log("ooooo")
+        logger.info('Got in to controller of login chefs');
         const { email, password } = req.body;
         const result = await checkForChef({ email, password }); // חזור עם תוצאה ולא json ישירות
         return res.status(200).send(result);
