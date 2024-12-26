852e7b8bfe12eadfa7e71f156d1aac6a88a22b5a
RuchamaWilk <ruchamawilk@gmail.com>
2024-12-26 14:12:19 +0200

    add logger to chef controller

--- server/controllers/chefController.js
+++ server/controllers/chefController.js
@@ -2,27 +2,26 @@
 const express = require('express');
 const router = express.Router();
 const logger = require('../services/loggerService');
-
-
 const { getChefs, checkForChef } = require('../services/chefService');
 
 router.get('/', async (req, res, next) => {
     try {
-        logger.info('Got in to controller of chefs');
+        logger.info('callibg getChefs');
         const chefs = await getChefs();
+        logger.info('success with getChefs');
         return res.status(200).send(chefs);
     } catch (err) {
-        next(err);  // טיפול בשגיאות
+        next(err); 
     }
 });
 
 router.post('/login', async (req, res, next) => {
 
     try {
-        console.log("ooooo")
-        logger.info('Got in to controller of login chefs');
         const { email, password } = req.body;
-        const result = await checkForChef({ email, password }); // חזור עם תוצאה ולא json ישירות
+        logger.info(`Calling checkForChef with email: ${email} and password: ${password}`);
+        const result = await checkForChef({ email, password }); 
+        logger.info('success with checkForChef');
         return res.status(200).send(result);
     } catch (err) {
         next(err);
