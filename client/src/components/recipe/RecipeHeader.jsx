54f8160b839e7740981080eb44ca895d967fe35a
RuchamaWilk <ruchamawilk@gmail.com>
2025-01-01 20:20:18 +0200

    height of header

--- client/src/components/recipe/RecipeHeader.jsx
+++ client/src/components/recipe/RecipeHeader.jsx
@@ -5,9 +5,10 @@ import PropTypes from 'prop-types';
 const RecipeHeader = ({ image, name }) => {
   return (
     <Box sx={{ position: 'relative' }}>
-      <CardMedia component="img" height="250" image={image} alt={name} sx={{ filter: "brightness(50%)" }} />
-      <Box sx={{ color: "#EEEDEB", position: 'absolute', top: '7%', left: '5%', textShadow: '-3px 1px 8px #939185' }}>
-        <Typography variant="h3">
+      <CardMedia component="img"   image={image} alt={name} sx={{ filter: "brightness(50%)" , width: '100%', // make the image responsive by making it take 100% width
+          height: 'auto', objectFit: 'cover', maxHeight: '300px'}} />
+      <Box sx={{ color: "#EEEDEB", position: 'absolute', top: '70%', left: '5%', textShadow: '-3px 1px 8px #939185' }}>
+        <Typography sx={{ fontSize: { xs: '1.5rem', sm: '2.5rem', md: '3rem' } }} >
           {name}
         </Typography>
       </Box>
