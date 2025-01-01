4cd7ca661e669a54a1346bee21e86495d02d244f
RuchamaWilk <ruchamawilk@gmail.com>
2025-01-01 20:19:36 +0200

    no prop for ai

--- client/src/components/recipe/RecipeAIBox.jsx
+++ client/src/components/recipe/RecipeAIBox.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { Box, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
 
-const RecipeAIBox = ({ aiSuggestion }) => {
+const RecipeAIBox = () => {
   return (
     <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'relative' }}>
       <Box 
         sx={{ width: "50vw", height: "20vh", backgroundColor: '#EEEDEB', borderRadius: '4px', marginTop: 2, padding: 2 }}>
         <Typography variant="h6">AI Suggestion</Typography>
-        <Typography variant="body1">{aiSuggestion}</Typography>
       </Box>
     </Box>
   );
