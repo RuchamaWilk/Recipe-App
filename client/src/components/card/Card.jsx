67020cec3255265d942b9ed4988344d0ecc57744
RuchamaWilk <ruchamawilk@gmail.com>
2025-01-01 13:59:44 +0200

    card setUp with raiting and level

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -5,10 +5,11 @@ import CardActions from '@mui/material/CardActions';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
 import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import Rating from '@mui/material/Rating';
+
 
 const Card = ({ recipe }) => {
     const navigate = useNavigate();
@@ -19,50 +20,53 @@ const Card = ({ recipe }) => {
 
     return (
         <CardUi sx={{
-                width: 320, height: 200, display: 'flex', flexDirection: 'column',
-                position: 'relative', 
-                borderRadius: "12px",
-                cursor: 'pointer',
-                transition: 'all 0.3s ease', 
-                '&:hover': {
-                backgroundColor: 'rgba(0, 0, 0, 0.1)', 
-                transform: 'scale(1.05)', 
-              }, }}>
+            border: "1px solid #ada6a7",
+            width: 320, height: 200, display: 'flex', flexDirection: 'column',
+            position: 'relative',
+            borderRadius: "12px",
+            cursor: 'pointer',
+            transition: 'all 0.3s ease',
+            backgroundColor: '#ebebeb',
+            '&:hover': {
+                transform: 'scale(1.03)',
+                boxShadow: "4px solid black"
+            },
+        }}>
             <CardMedia
-                onClick={showRecipe}
                 component="img"
-                sx={{ height: '66%' }}
                 image={recipe.image}
                 alt={recipe.name}
+                sx={{ height: '60%' }}
             />
-            <CardContent sx={{ flex: '1 0 auto', display: 'flex', flexDirection: 'column',justifyContent: 'space-between', padding: '8px 16px' }}>
-                <div>
-                    <Typography variant="h6" noWrap>{recipe.name}</Typography>
-                    <Typography /*variant="body2"*/ color="text.secondary">
-                        {`${recipe.avgTime} Min`}
-                    </Typography>
-                </div>
-            </CardContent>
-            <CardActions disableSpacing sx={{ position: 'absolute', bottom: 8, right: 8, padding: 0 ,gap: 1}}>
-                <IconButton aria-label="add to favorites" sx={{ padding: 0, fontSize: 'small' }}>
-                    <FavoriteIcon fontSize="small" />
-                </IconButton>
-                <IconButton aria-label="share" sx={{ padding: 0, fontSize: 'small' }}>
-                    <ShareIcon fontSize="small" />
+            <CardActions disableSpacing sx={{position: 'absolute', margin: "-10px"}}>
+                <IconButton  sx={{ color: 'transparent',stroke: 'white',
+                    '&:hover': {transform: 'scale(1.05)',color: "red", stroke: 'red'},}}>
+                    <FavoriteIcon fontSize="large"  />
                 </IconButton>
             </CardActions>
+            <CardContent onClick={showRecipe} sx={{ flex: '1 0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', padding: '2px 4px' }}>
+                <div>
+                    <Typography variant="h6" noWrap>{recipe.name}</Typography>
+                    <Typography color="text.secondary" sx={{ lineHeight: 0.70, paddingTop: "4px" }}>
+                        {`${recipe.avgTime} Min | Difficulty: ? | Chef: ${recipe.chefId}`}
+                    </Typography>
+                    <Rating name="no-value" value={3} readOnly  sx= {{paddingBlock: "2px",fontSize: "1.1rem", paddingTop: "8px"}}/>
+
+                    
+                </div>
+            </CardContent>
         </CardUi>
     );
 };
 
 Card.propTypes = {
     recipe: PropTypes.shape({
-      recipeId: PropTypes.string.isRequired,  // מזהה המתכון (במספר)
-      image: PropTypes.string.isRequired,     // כתובת התמונה (מחרוזת)
-      name: PropTypes.string.isRequired,      // שם המתכון (מחרוזת)
-      avgTime: PropTypes.number.isRequired,  // זמן ממוצע ב-Min (מספר)
+        recipeId: PropTypes.string.isRequired,  // מזהה המתכון (במספר)
+        image: PropTypes.string.isRequired,     // כתובת התמונה (מחרוזת)
+        name: PropTypes.string.isRequired,      // שם המתכון (מחרוזת)
+        avgTime: PropTypes.number.isRequired,  // זמן ממוצע ב-Min (מספר)
+        chafId: PropTypes.string.isRequired
     }).isRequired, // שדה recipe חייב להיות תמיד
-  
-  };
-  
+};
+
 export default Card;
