5dbad2f152596a9be0772e94b7751171e789b504
RuchamaWilk <ruchamawilk@gmail.com>
2025-01-05 11:31:40 +0200

    add function pre save user and checkpassword

--- server/models/chef.js
+++ server/models/chef.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const chefSchema = new mongoose.Schema({
     profileImage: { type: String },
@@ -6,6 +8,28 @@ const chefSchema = new mongoose.Schema({
     userName: { type: String, required: true },
     emailAddress: { type: String, required: true, unique: true } 
 });
+
+userSchema.pre('save', function(next) {
+    var user = this;
+
+    if (!user.isModified('password')) return next();
+
+    bcrypt.hash(user.password, saltRounds, function (err, hash) {
+        if (err) { return next(err); }
+        user.password = hash;
+        next();
+    });
+});
+
+userSchema.methods.checkPassword = function(candidatePassword) {
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+            if (err) return reject(err);
+            resolve(isMatch);
+        });
+    })
+};
+
 
 const Chef = mongoose.model('chefs', chefSchema); 
 
