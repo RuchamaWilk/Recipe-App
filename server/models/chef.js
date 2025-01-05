27a68bc1ce21de822770e8243d88a54839e2874f
RuchamaWilk <ruchamawilk@gmail.com>
2025-01-05 11:22:07 +0200

    delete id from schema

--- server/models/chef.js
+++ server/models/chef.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const chefSchema = new mongoose.Schema({
-    chefId: { type: String, required: true, unique: true },
     profileImage: { type: String },
     password: { type: String, required: true },
     userName: { type: String, required: true },
