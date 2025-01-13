02b4f1fadf2591a18be50b218274ea1f33e2793c
RuchamaWilk <ruchamawilk@gmail.com>
2025-01-13 13:44:53 +0200

    add rout favorite and delete signin and aignup user

--- client/src/routers/AppRouters.jsx
+++ client/src/routers/AppRouters.jsx
@@ -4,11 +4,10 @@ import HomePage from '../pages/home-page/HomePage';
 import Recipe from '../pages/recipe-page/recipePage';
 import RecipeByCategory from '../pages/recipes-by-category-page/RecipeByCategoryPage'
 import AddRecipePage from '../pages/add-recipe-page/AddRecipePage';
-import SignInPage from '../pages/sign-in-page/SignInPage';
 import ErrPage from '../pages/err-page/errPage'
-import SignUpPage from '../components/sign-up/SignUp'
 import SignUpPageChef from '../pages/sign-up-page/SignUpPageChef'
 import ProtectedRoute from './ProtectedRouts';
+import MyFavorite from  '../pages/my-favorite-page/MyFavoritePage'
 
 
 function AppRouters() {
@@ -17,16 +16,19 @@ function AppRouters() {
       <Route path="/" element={<HomePage />} />
       <Route path="/recipe/:id" element={<Recipe />} />
       <Route path="/category/:category" element={<RecipeByCategory />} />
-      <Route path="nice"  />
-      <Route path="/log-in" element={<SignInPage />} />
-      <Route path="/user-sign-up" element={<SignUpPage/>} />
       <Route path="/chef-sign-up" element={<SignUpPageChef/>} /> 
       <Route path="*" element={<ErrPage errCode= "400" />} />
-      <Route 
-        path="/add-recipe" 
+      <Route path="/add-recipe" 
         element={
           <ProtectedRoute allowedTypes={['chef']}>
             <AddRecipePage />
+          </ProtectedRoute>
+        } 
+      />
+      <Route path="/favorite" 
+        element={
+          <ProtectedRoute allowedTypes={['user']}>
+            <MyFavorite />
           </ProtectedRoute>
         } 
       />
